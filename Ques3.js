6d517ed87334e95f8a2c012e7f93bf5cc047e3aa
[ShreeyaLal] <[lalshreeya@gmail.com]>
2021-10-20 21:53:10 +0545

    third question solved and added.

---
+++ Ques3.js
@@ -0,0 +1,19 @@
+
+var ToDoArray = [];
+var check;
+
+do{
+    inTitle = prompt(" Add a task title");
+    inDescription = prompt(" Add a task description");
+    ToDoArray.push({title: inTitle, description: inDescription});
+    console.log("Task added successfully!");
+    check = prompt("more tasks(Y/N)?", "Y");
+    console.log(check.toUpperCase());
+}while (check.toUpperCase()=="Y")
+
+console.log("\n TO DO LIST");
+
+for ( var i in ToDoArray){
+    console.log("Title: ", ToDoArray[i].title);
+    console.log("Description: ", ToDoArray.description);
+    }
